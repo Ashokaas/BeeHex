aa584304f95f24b0aff2eac3354b1b5344e08610
Ashokas <99681959+Ashokaas@users.noreply.github.com>
2025-05-17 23:09:11 +0200

    fix navbar lag

--- src/components/bottom_navbar/bottom_navbar.tsx
+++ src/components/bottom_navbar/bottom_navbar.tsx
@@ -1,53 +1,50 @@
 "use client"
 
 import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+
 import styles from './bottom_navbar.module.css';
 import 'material-symbols';
 
 import Cookies from 'js-cookie';
 
-// en fait pour régler mon provlème il faut créer un élément pour chaque bouton mais la je vais me coucher
+// en fait pour régler mon problème il faut créer un élément pour chaque bouton mais là je vais me coucher
+
 function MyAccountButton() {
   return (
-    <a href="/my_account">
+    <Link className={styles.a} href="/my_account">
       <p>Mon compte</p>
       <span className="material-symbols-rounded">account_circle</span>
-    </a>
+    </Link>
   );
 }
 
 function LoginRegisterButton() {
   return (
-    <a href="/login_register">
+    <Link className={styles.a} href="/login_register">
       <p>Se connecter</p>
       <span className="material-symbols-rounded">login</span>
-    </a>
+    </Link>
   );
 }
 
 
 function ShowLoginOrMyAccount() {
   const [loggedIn, setLoggedIn] = useState(false);
-  
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    if (Cookies.get('token')) {
-      setLoggedIn(true);
-    }
-    const handleCookieChange = () => {
-      setLoggedIn(Cookies.get('token') ? true : false);
-    };
+    setIsMounted(true);
+    const checkLogin = () => setLoggedIn(!!Cookies.get('token'));
+    checkLogin();
 
-    window.addEventListener('cookieChange', handleCookieChange);
-
-    
+    window.addEventListener('cookieChange', checkLogin);
+    return () => window.removeEventListener('cookieChange', checkLogin);
   }, []);
 
-  return (
-    <>
-      {loggedIn ? <MyAccountButton /> : <LoginRegisterButton />}
-    </>
-  );
+  if (!isMounted) return null; // Empêche le rendu côté serveur
+
+  return loggedIn ? <MyAccountButton /> : <LoginRegisterButton />;
 }
 
 
@@ -56,22 +53,22 @@ function ShowLoginOrMyAccount() {
 const BottomNavBar = () => {
   return (
     <div id={styles.bottom_nav_bar}>
-    <a href="/">
+    <Link className={styles.a} href="/">
       <p>Accueil</p>
       <span className="material-symbols-rounded">home</span>
-    </a>
-    <a href="/game_mode">
+    </Link>
+    <Link className={styles.a} href="/game_mode">
       <p>Jouer</p>
       <span className="material-symbols-rounded">swords</span>
-    </a>
-    <a href="/rank">
+    </Link>
+    <Link className={styles.a} href="/rank">
       <p>Classement</p>
       <span className="material-symbols-rounded">leaderboard</span>
-    </a>
-    <a href="#">
+    </Link>
+    <Link className={styles.a} href="#">
       <p>Historique</p>
       <span className="material-symbols-rounded">replay</span>
-    </a>
+    </Link>
     <ShowLoginOrMyAccount />
   </div>
   );
